ffbbcb81bfa5a7d3ede6a5ec51bf5330e19197d2
Geetanjali <165178774+GeetanjaliMahobiya@users.noreply.github.com>
2026-08-18 21:00:18 +0530

    commit first

---
+++ CalculatorPy/code.py
@@ -0,0 +1,27 @@
+a = float(input("Enter the 1st Number: "))
+b = float(input("Enter the 2nd Number: "))
+
+op = input("Enter operator(+,-,*,/,%,**): ")
+
+if op == '+':
+    print(a+b)
+elif op == '-':
+    print(a-b)
+elif op == '*':
+    print(a*b)
+elif op == '/':
+    print(a/b)
+elif op == '%':
+    print(a%b)
+elif op == '**':
+    print(a**b)
+else:
+    print("INVALID OPERATION")
+
+
+           
+
+
+
+
+
